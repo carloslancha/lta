9462021aec2260465ab607caa2082b9c75ea68a8
Carlos Lancha <carlos.lancha@liferay.com>
2019-11-27 05:21:53 +0100

    API: Add nextMatch api

--- server/src/resolvers/Match.js
+++ server/src/resolvers/Match.js
@@ -18,9 +18,15 @@ function poule(parent, args, context) {
 	return context.prisma.match({ id: parent.id }).poule()
 }
 
+async function nextMatch(parent, args, context) {
+    const matches = await context.prisma.match({ id: parent.id }).poule().matches()
+    return matches.find(match => match.order === (parent.order +1))
+}
+
 module.exports = {
     assaults,
     createdBy,
+    nextMatch,
     player1,
     player2,
     poule,
